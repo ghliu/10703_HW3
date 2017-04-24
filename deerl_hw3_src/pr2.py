0b009bbb00c3eb83dd0094a6fd9dc11ced77fa0e
jeremy89183 <jeremy89183@gmail.com>
2017-04-25 06:19:51 +0800

    Addded DAGGER

--- deerl_hw3_src/pr2.py
+++ deerl_hw3_src/pr2.py
@@ -1,6 +1,7 @@
 from deeprl_hw3 import imitation
 import gym
 import argparse
+import numpy as np
 
 expert = imitation.load_model('CartPole-v0_config.yaml', 'CartPole-v0_weights.h5f')
 env = gym.make('CartPole-v0')
@@ -27,3 +28,20 @@ print("----- evaluate our cloned model -----")
 imitation.test_cloned_policy(harder_env, model, render = False)
 print("----- evaluate our expert model -----")
 imitation.test_cloned_policy(harder_env, expert, render = False)
+
+print("===== Bonus DAGGER1 =====")
+model = imitation.load_model('CartPole-v0_config.yaml')
+dagger_num_episode = 20
+observations, expert_actions = imitation.generate_expert_training_data(expert, env, num_episodes = 1, render = False)
+for t in range(dagger_num_episode):
+    #learn from data
+    imitation.behavior_cloning(model, observations, expert_actions, num_of_epochs=5)
+    #generate trajectory from model, NOT expert here
+    traj, _ = imitation.generate_expert_training_data(model, env, num_episodes = 1, render = False)
+    act = expert.predict_on_batch(traj)
+    one_hot = np.zeros((len(act), 2))
+    for i in range(len(act)):
+        one_hot[i][np.argmax(act[i])] = 1.
+    observations = np.append(observations, traj, axis=0)
+    expert_actions = np.append(expert_actions, one_hot, axis=0)
+imitation.test_cloned_policy(env, model, render = False)
