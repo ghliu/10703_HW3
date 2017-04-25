09591344cf96889d6de18a808e3844bc28a83b84
ghliuLAB <guanhorl@andrew.cmu.edu>
2017-04-24 20:34:49 -0400

    update problem 1

--- deerl_hw3_src/pr1.py
+++ deerl_hw3_src/pr1.py
@@ -3,6 +3,7 @@
 import numpy as np
 import gym
 import time
+import pickle
 
 import argparse
 
@@ -14,7 +15,7 @@ from ipdb import set_trace as debug
 
 parser = argparse.ArgumentParser(description='DRC 10703 Hw3')
 
-parser.add_argument('--prob', default='1', type=str, help='')
+parser.add_argument('--controller', default='lqr', type=str, help='')
 parser.add_argument('--env', default='TwoLinkArm-v0', type=str, help='')
 parser.add_argument('--tN', default=100, type=int, help='')
 parser.add_argument('--max_iter', default=100, type=int, help='')
@@ -32,14 +33,13 @@ goal_q = env.goal_q.copy()
 
 # build controller
 select_action = {
-	'1': lambda env, sim_env: calc_lqr_input(
+	'lqr': lambda env, sim_env: calc_lqr_input(
 		env,sim_env,debug_flag=args.debug),
-	'2': lambda env, sim_env: calc_ilqr_input(
-		env,sim_env,debug_flag=args.debug, max_iter=args.max_iter, tN=args.tN),
-	'2-useLM': lambda env, sim_env: calc_ilqr_input(
-		env,sim_env,debug_flag=args.debug, max_iter=args.max_iter,
-		tN=args.tN, useLM=True),
-	'2-mpc': lambda env, sim_env: calc_mpc_input(
+	'ilqr': lambda env, sim_env: calc_ilqr_input(
+		env,sim_env,debug_flag=args.debug, max_iter=args.max_iter, tN=args.tN, useLM=False),
+	'ilqr-speedup': lambda env, sim_env: calc_ilqr_input(
+		env,sim_env,debug_flag=args.debug, max_iter=args.max_iter, tN=args.tN, useLM=True),
+	'mpc': lambda env, sim_env: calc_mpc_input(
 		env,sim_env,debug_flag=args.debug, max_iter=args.max_iter,
 		tN=args.tN, useLM=True),
 }.get(args.prob)
@@ -56,7 +56,7 @@ while True:
 
 	action = select_action(env, sim_env)
 
-	obs, reward, is_done, _ = env.step(action)
+	obs, reward, is_done, meta = env.step(action)
 	
 	action_history.append(action)
 	state_history.append(obs)
@@ -67,8 +67,7 @@ while True:
 	if np.linalg.norm(goal_q-env.position) < args.tol:
 		break
 
-#debug()
-pkl = open('result-prob%s-%s.pkl'%(opts.prob,opts.env))
+pkl = open('result-prob%s-%s.pkl'%(args.prob,args.env),'wb')
 pickle.dump(state_history, pkl)
 pickle.dump(action_history, pkl)
 pickle.dump(reward_history, pkl)
--- deerl_hw3_src/run.sh
+++ deerl_hw3_src/run.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+# problem 1 (LQR)
+python3 pr1.py --controller lqr --env TwoLinkArm-v0
+python3 pr1.py --controller lqr --env TwoLinkArm-limited-torque-v0
+python3 pr1.py --controller lqr --env TwoLinkArm-v1
+python3 pr1.py --controller lqr --env TwoLinkArm-limited-torque-v1
+
+# problem 1 (iLQR)
+python3 pr1.py --controller ilqr --env TwoLinkArm-v0
+python3 pr1.py --controller ilqr --env TwoLinkArm-v1
+
+# problem 1 (extra credit: Speedup & MPC)
+python3 pr1.py --controller ilqr-speedup --env TwoLinkArm-v0
+python3 pr1.py --controller ilqr-speedup --env TwoLinkArm-v1
+python3 pr1.py --controller mpc --env TwoLinkArm-v0
+python3 pr1.py --controller mpc --env TwoLinkArm-v1
+
+# problem 2 (Imitation module, include extra credit on DAGGER)
 python3 pr2.py -n_ep 1 > ./output/pr2_1
 python3 pr2.py -n_ep 10 > ./output/pr2_10
 python3 pr2.py -n_ep 50 > ./output/pr2_50
 python3 pr2.py -n_ep 100 > ./output/pr2_100
 
+# problem 3 (REINFORCE)
 python3 pr3.py -type linear > ./output/pr3_linear
 python3 pr3.py -type default > ./output/pr3_default
